2a7aaa849b0271b087e87361729eb5e39a5d1a6b
Dinesh B S <dineshbs444@gmail.com>
2021-06-20 20:46:08 +0530

    Added changes

--- block.js
+++ block.js
@@ -1,7 +1,7 @@
 /**
  * Import crypto-js/SHA256 library
  */
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require('crypto-js/sha256.js');
 
 /**
  * Class with a constructor for block 
@@ -9,7 +9,7 @@ const SHA256 = require('crypto-js/sha256');
 class Block {
 
 	constructor(data){
-		this.id = 0;
+		this.id = 1;
         this.nonce = 144444;
       	this.body = data;
       	this.hash = "";
@@ -26,7 +26,18 @@ class Block {
   	generateHash() {
       	// Use this to create a temporary reference of the class object
       	let self = this;
+
         //Implement your code here
+        var promise = new Promise(function(resolve, reject) {
+            self.hash = SHA256(JSON.stringify(self));
+            if(self.hash) {
+                resolve(self);
+            } else {
+                reject(Error("It didn't work"));
+            }
+        });
+
+        return promise; 
         
     }
 }
